28703cf6d879f9945ee5fb32f21889dc011f53ab
Kris Jordan <krisjordan@gmail.com>
2023-02-26 17:43:20 -0500

    Initial Commit

---
+++ version_check.py
@@ -0,0 +1,6 @@
+# Add any notes or additional tests from Tutorial Overview here.
+# This file is provided as an example.
+# To run it, try `python3 -m version_check` in the terminal.
+
+import sqlalchemy
+print(sqlalchemy.__version__)
